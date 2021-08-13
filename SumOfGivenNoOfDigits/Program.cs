4718aae5f4f64e4d0b8bad53c515a802b4c3a685
nani.madhu1117 <nani.madhu1117@gmail.com>
2021-08-13 11:35:19 +0530

    Add project files.

---
+++ SumOfGivenNoOfDigits/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace SumOfGivenNoOfDigits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter input :");
+            int a = int.Parse(Console.ReadLine());
+            int sum = 0;
+            int rem = 0;
+            int b = a;
+
+            while (a!=0)
+            {
+                rem = a % 10;
+                sum = sum + rem;
+                a = a / 10;
+            }
+            Console.WriteLine("Sum of {0} is {1}:",b,sum);
+        }
+    }
+}
